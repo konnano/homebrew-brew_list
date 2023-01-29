9af5ed23768e5696379d912fcf27919ad5d32e40
konnano <73874687+konnano@users.noreply.github.com>
2023-01-29 23:58:01 +0900

    Update brew_list.rb

--- brew_list.rb
+++ brew_list.rb
@@ -1,13 +1,13 @@
 class BrewList < Formula
   desc "Enhanced brew list"
   homepage "https://github.com/konnano/brew_list"
-  url "https://github.com/konnano/brew_list/releases/download/v1.16.2/brew_list"
-  version "1.16.2"
-  sha256 "635fe56b63067b2f322e8ffb98c012f3bbde84bb426a423464dd6c951b065d9f"
+  url "https://github.com/konnano/brew_list/releases/download/v1.16.3/brew_list"
+  version "1.16.3"
+  sha256 "0c7fde1bf1879d9b48c7df658924da7aa4accc446cf84ec036190d015b54aee5"
 
   bottle do
-    root_url "https://github.com/konnano/homebrew-brew_list/releases/download/v1.16.2"
-    sha256 cellar: :any_skip_relocation, all: "d27d9ba9b01a808115eafe518d64cce8afa516ccf82335bb514d599ad071563f"
+    root_url "https://github.com/konnano/homebrew-brew_list/releases/download/v1.16.3"
+    sha256 cellar: :any_skip_relocation, all: "5ecb0cd46e49c73101039e4f5c65372f57b1061dbca633509581a865f9649fef"
   end
 
   def install
