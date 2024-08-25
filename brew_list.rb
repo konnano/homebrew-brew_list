17d295279e9314170efe0092e9e5120081160229
konnano <73874687+konnano@users.noreply.github.com>
2024-08-25 18:38:29 +0900

    Update brew_list.rb

--- brew_list.rb
+++ brew_list.rb
@@ -1,14 +1,14 @@
 class BrewList < Formula
   desc "Enhanced brew list"
   homepage "https://github.com/konnano/brew_list"
-  url "https://github.com/konnano/brew_list/releases/download/v1.26.9/bl"
-  version "1.26.9"
-  sha256 "017305fdc125fe577d7349000c14dd9a7adaeb879fed79edc0bc00e517710c1c"
+  url "https://github.com/konnano/brew_list/releases/download/v1.27.0/bl"
+  version "1.27.0"
+  sha256 "387974ff987e7ca7207fd0399369e859ab4ce98e1020deac51e1c2b25f56d34a"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/konnano/homebrew-brew_list/releases/download/v1.26.9"
-    sha256 cellar: :any_skip_relocation, all: "ae288cd4654c0f6e6f164f0459384cb874c0266bc23bcd0c184d190160a5a000"
+    root_url "https://github.com/konnano/homebrew-brew_list/releases/download/v1.27.0"
+    sha256 cellar: :any_skip_relocation, all: "02e79d5d94d8c2f1c938baefc13c781e6c3c00d863065cc9e77a0e2131edc6bf"
   end
 
   def install
